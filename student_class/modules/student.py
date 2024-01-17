6b00293cb9fd3a9f23537d61b5500f9dbc2c4ff7
Oleksii Koliukh <oleksii.koliukh@gmail.com>
2024-01-17 15:57:18 +0000

    Part A completed

--- student_class/modules/student.py
+++ student_class/modules/student.py
@@ -1,1 +1,10 @@
-
+class Student:
+    def __init__(self, name, cohort) -> None:
+        self.name = name
+        self.cohort = cohort
+    
+    def talk(self):
+        return "I can talk!"
+    
+    def say_favourite_language(self, lang):
+        return f"I love {lang}"
